b9b9b83089123944af6b62f21579609b1ab4ac5b
RiaBhasin <43845217+RiaBhasin@users.noreply.github.com>
2021-01-25 19:09:21 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+function university_files() {
+    wp_enqueue_style('custom_font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+    if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+        wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    }
+    else {
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assests/styles.css'));
+    }
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+        // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        // register_nav_menu('footerLocationOne', 'Footer Location One');
+        // register_nav_menu('footerLocationTwo', 'Footer Location Two');
+        add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
+
+function university_post_types() {
+    register_post_type('event', array(
+        'public' => true
+    ));
+}
+
+add_action('init','university_post_types');
